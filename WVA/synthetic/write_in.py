7df90bbe09f07cd9d5eaac991f022fe3336fe8cf
Chet Cgoerzen <chet.goerzen@gmail.com>
2021-07-06 12:52:42 -0400

    Add WVA

---
+++ WVA/synthetic/write_in.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+maxPer = 12.3
+
+nx = 36
+ny = 36
+nz = 10
+goxd = -38.4
+gozd = 177.945
+dvxd = 0.006
+dvzd = 0.009
+nsrc = 1526
+weight = 3.0
+damp = 1.0
+sablayers = 3
+minvel = 0.1
+maxvel = 3.0
+niter = 1
+synthetic = 1
+
+
+with open("DSurfTomo.in", "w") as nFile:
+    nFile.write("cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc\n")
+    nFile.write("c INPUT PARAMETERS\n")
+    nFile.write("cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc\n")
+    nFile.write(f"DSurfTomoJoint.dat                    c: data file\n")
+    nFile.write(f"{nx} {ny} {nz}                   c: nx ny nz (grid number in lat lon depth direction)\n")
+    nFile.write(f"{goxd}  {gozd}                   c: goxd gozd (upper left point,[lat,lon])\n")
+    nFile.write(f"{dvxd} {dvzd}                    c: dvxd dvzd\n")
+    nFile.write(f"{nsrc}                           c: max(sources, receivers)\n")
+    nFile.write(f"{weight} {damp}                  c: weight damp\n")
+    nFile.write(f"{sablayers}                      c: sablayers\n")
+    nFile.write(f"{minvel} {maxvel}                c: min, max velocity\n")
+    nFile.write(f"{niter}                          c: number of iterations\n")
+    nFile.write(f"0.2                              c: sparsity fraction\n")
+    nFile.write(f"{len(np.arange(1.0, 13.0, 1.0))}                               c: kmaxRc (followed by periods)\n")
+    for period in np.arange(1.0, 13.0, 1.0):
+        nFile.write(f"{period:1.1f} ")
+    nFile.write("c: periods\n")
+    nFile.write(f"{len(np.arange(1.0, 16.0, 1.0))}                                c: kmaxRg\n")
+    for period in np.arange(1.0, 16.0, 1.0):
+        nFile.write(f"{period:1.1f} ")
+    nFile.write("c: periods\n")
+    nFile.write(f"0                                c: kmaxLc\n")
+    nFile.write(f"0                                c: kmaxLg\n")
+    nFile.write(f"{synthetic}                                c: synthetic flag(0:real data,1:synthetic)\n")
+    nFile.write(f"0.02                             c: noise level\n")
+    nFile.write(f"1.5                              c: threshold\n")
+    nFile.write(f"1 200 50 c: vorotomo,ncells,nrelizations")
+    nFile.close()
+
